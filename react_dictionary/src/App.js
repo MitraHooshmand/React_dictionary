e98574ab5e24d66e63bcdb6820761e56ea779cca
mita4824 <87270333+mita4824@users.noreply.github.com>
2022-01-08 20:23:40 +0330

    add test btn

--- react_dictionary/src/App.js
+++ react_dictionary/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        hi
-        
+        <a href="#" className="btn btn-primary">test</a>
       </header>
     </div>
   );
